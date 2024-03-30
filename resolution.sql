7d42f15f5b5028a96c06fb02a0ec938e1ca613fa
Nícolas Carloto <90424448+senhorN@users.noreply.github.com>
2024-03-30 03:32:55 -0300

    Update resolution.sql

--- resolution.sql
+++ resolution.sql
@@ -6,27 +6,27 @@ from Filmes
 --exc 2
 select Nome  
      , Ano
-	 , Duracao
+     , Duracao
 from Filmes
 
 --exc3
 select Nome  
      , Ano
-	 , Duracao
+     , Duracao
 from Filmes
 where Nome = 'De Volta para o Futuro'
 
 --exc4
 select Nome  
      , Ano
-	 , Duracao
+     , Duracao
 from Filmes
 where ano = 1997
 
 --exc5
 select Nome  
      , Ano
-	 , Duracao
+     , Duracao
 from Filmes
 where ano > 2000
 
@@ -34,13 +34,13 @@ where ano > 2000
 
 select Nome  
      , Ano
-	 , Duracao
+     , Duracao
 from Filmes
 where Duracao > 100 and Duracao  < 150 
 
 --ex7 
 select Ano
-	 ,COUNT(Ano)as Quantidade	 
+     ,COUNT(Ano)as Quantidade	 
 from Filmes
 group by Ano
 order by Quantidade	 desc
@@ -48,23 +48,23 @@ order by Quantidade	 desc
 --exc8
 select PrimeiroNome
      , UltimoNome
-	 , Genero
+     , Genero
 from Atores
 where Genero = 'M'
 
 --ex9 
 select PrimeiroNome
      , UltimoNome
-	 , Genero
+     , Genero
 from Atores
 where Genero = 'F'
 
 --exc 10
 select  Filmes.Nome
      ,  Generos.Genero
-from  Filmes
-     , FilmesGenero
-	 , Generos		
+from    Filmes
+     ,  FilmesGenero
+     ,  Generos		
 where Filmes.Id = FilmesGenero.IdFilme
 and   FilmesGenero.IdGenero = Generos.Id
 
@@ -72,9 +72,9 @@ and   FilmesGenero.IdGenero = Generos.Id
 --exc 11
 select  Filmes.Nome
      ,  Generos.Genero
-from  Filmes
-     , FilmesGenero
-	 , Generos		
+from    Filmes
+     ,  FilmesGenero
+     ,  Generos		
 where Filmes.Id = FilmesGenero.IdFilme
 and   FilmesGenero.IdGenero = Generos.Id
 and Generos.Genero = 'Mistério'
@@ -83,10 +83,10 @@ and Generos.Genero = 'Mistério'
 --exc12
 select f.Nome
      , ator.PrimeiroNome
-	 , ator.UltimoNome
-	 , elenco.Papel
+     , ator.UltimoNome
+     , elenco.Papel
 from   Filmes f
      , ElencoFilme elenco
-	 , Atores ator
+     , Atores ator
 where f.Id = elenco.IdFilme
 and   elenco.IdAtor = ator.Id
